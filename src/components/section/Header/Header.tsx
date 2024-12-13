c771b424ffcce37666f9e20c6d3538752ea9f821
HenriqueFMA <carolinahenriquef@gmail.com>
2024-12-13 18:56:19 -0300

    add react-icons dependency and enhance Avatar component with hover message and remodeling of the Header

--- src/components/section/Header/Header.tsx
+++ src/components/section/Header/Header.tsx
@@ -18,25 +18,35 @@ const Header = ({ setScreen, screen }: Props) => {
   const [button2, setButton2] = useState("Experiences");
   const [button3, setButton3] = useState("Projects");
   const [visible, setVisible] = useState(false);
+  const [fade, setFade] = useState(false); // Controle para a transição
+
 
   const menuRef = useRef<HTMLDivElement | null>(null);
   console.log(screen + " header");
 
   const handleButtonClick = () => {
-    if (name === "PT") {
-      setName("EN");
-      setButton1("Skills");
-      setButton2("Experiences");
-      setButton3("Projects");
-      setImagePath("/src/assets/bandeiraUSA.png");
-    } else {
-      setName("PT");
-      setButton1("Habilidades");
-      setButton2("Experiências");
-      setButton3("Projetos");
-      setImagePath("/src/assets/bandeiranacionalbrasil.png");
-    }
+    setFade(true); // Inicia o fade-out
+
+    setTimeout(() => {
+      // Após o fade-out, muda o conteúdo
+      if (name === "PT") {
+        setName("EN");
+        setButton1("Skills");
+        setButton2("Experiences");
+        setButton3("Projects");
+        setImagePath("/src/assets/bandeiraUSA.png");
+      } else {
+        setName("PT");
+        setButton1("Habilidades");
+        setButton2("Experiências");
+        setButton3("Projetos");
+        setImagePath("/src/assets/bandeiranacionalbrasil.png");
+      }
+
+      setFade(false); // Inicia o fade-in
+    }, 300); // Tempo suficiente para o fade-out terminar (coincide com a duração da animação)
   };
+
 
   const handleButtonClickNavigate = (screen: string) => {
     setScreen(screen);
@@ -63,120 +73,141 @@ const Header = ({ setScreen, screen }: Props) => {
   }, []);
 
   return (
-    <div className="flex section mx-auto px-4 md:px-6 lg:px-8">
-      <header className="flex items-center justify-between h-20 w-full px-4 md:px-6 lg:px-8">
-        <div className="flex-shrink-0">
-          <AvatarDemo />
-        </div>
+    <div className="section mx-auto px-4 md:px-6 lg:px-8">
+      <header className="flex justify-between h-20 w-full shrink-0 items-center px-4 md:px-6 lg:justify-around">
 
-        <div className="flex items-center gap-4">
-          <div className="hidden md:flex gap-3">
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded select-none"
+            <div className="flex-shrink-0 hover:scale-95  ">
+              <AvatarDemo />
+            </div>
+
+        
+           <div className=" gap-2 md:flex hidden">
+           <button
+              className="bg-black border-x-0 border-t-0  border-gray-300 text-white px-4 py-2  select-none hover:scale-x-100 hover:text-blue-500 "
               onClick={() => handleButtonClickNavigate("home")}
             >
-              <FaHome />
+              <FaHome size={24} />
             </button>
             <button
-              className="bg-blue-500 text-white px-4 py-2 rounded select-none"
+              className="bg-black border-x-0 border-t-0  border-gray-300 text-white px-4 py-2  select-none hover:scale-x-100 hover:text-blue-500     "
               onClick={() => handleButtonClickNavigate("Skills")}
             >
               {button1}
             </button>
             <button
-              className="bg-blue-500 text-white px-4 py-2 rounded select-none"
+              className="bg-black border-b-2 text-white px-4 py-2  border-x-0 border-t-0  border-gray-300 select-none hover:scale-x-100 hover:text-blue-500"
               onClick={() => handleButtonClickNavigate("Experiences")}
             >
               {button2}
             </button>
             <button
-              className="bg-blue-500 text-white px-4 py-2 rounded select-none"
+              className="bg-black border-b-2 text-white px-4 py-2  border-x-0 border-t-0  border-gray-300 select-none hover:scale-x-100 hover:text-blue-500"
               onClick={() => handleButtonClickNavigate("Projects")}
             >
               {button3}
             </button>
-          </div>
+            <button
+              onClick={handleButtonClick}
+              className="bg-black text-white px-4 py-2 rounded flex items-center select-none transition-all border-x-0 border-t-0  border-gray-300 focus:outline-none hover:scale-x-100 hover:text-blue-500 "
+            >
+              <img
+                src={imagePath}
+                alt="Language Flag"
+                className={`w-7 h-6 rounded-full clip-diagonal transition-transform duration-500 ${fade ? "-rotate-180 opacity-0" : "rotate-0 opacity-100"
+                  }`}
 
-          <button
-            onClick={handleButtonClick}
-            className="bg-gray-500 text-white px-4 py-2 rounded flex items-center select-none"
-          >
-            <img
-              src={imagePath}
-              alt="Language Flag"
-              className="w-7 h-6 rounded-full clip-diagonal"
-            />
-            {name}
-          </button>
+              />
+              <span className="ml-2">{name}</span>
+            </button>
+            </div>
+
+
+          <div className="flex md:hidden items-center gap-2">
+            
+            <button
+              onClick={handleButtonClick}
+              className="bg-black text-white px-4 py-2 rounded flex items-center select-none transition-all border-x-0 border-t-0  border-gray-300 focus:outline-none hover:scale-x-100 hover:text-blue-500 "
+            >
+              <img
+                src={imagePath}
+                alt="Language Flag"
+                className={`w-7 h-6 rounded-full clip-diagonal transition-transform duration-500 ${fade ? "-rotate-180 opacity-0" : "rotate-0 opacity-100"
+                  }`}
+
+              />
+              <span className="ml-2">{name}</span>
+            </button>
+            <div className="flex md:hidden items-center">
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded"
+              onClick={handleButtonClickVisible}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="27"
+                height="27"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="4" x2="20" y1="12" y2="12"></line>
+                <line x1="4" x2="20" y1="6" y2="6"></line>
+                <line x1="4" x2="20" y1="18" y2="18"></line>
+              </svg>
+            </button>
+            </div>
+
+           
         </div>
       </header>
 
-      <div className="flex md:hidden items-center gap-2">
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-          onClick={handleButtonClickVisible}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="27"
-            height="27"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="4" x2="20" y1="12" y2="12"></line>
-            <line x1="4" x2="20" y1="6" y2="6"></line>
-            <line x1="4" x2="20" y1="18" y2="18"></line>
-          </svg>
-        </button>
-
-        {visible && (
-          <div
-            ref={menuRef}
-            className="flex bg-black flex-col absolute shadow-slate-100 shadow-md py-4 rounded-lg mt-56 right-4 max-w-xs w-[50%] overflow-auto gap-0 align-middle items-center"
-          >
-            <div className="align-middle items-center flex gap-5">
-              <text className="text-white ml-9 text-2xl">Menu</text>
+      {visible && (
               <div
-                onClick={() => setVisible(false)} 
-                className="bg-black text-white"
+                ref={menuRef}
+                className="flex bg-black flex-col mt-0.5  absolute md:hidden shadow-slate-100 shadow-md py-4 rounded-lg  right-4 max-w-xs w-[50%] overflow-auto gap-0 align-middle items-center"
               >
-                <IoClose />
+                <div className="align-middle items-center flex gap-5">
+                  <text className="text-white ml-9 text-2xl">Menu</text>
+                  <div
+                    onClick={() => setVisible(false)}
+                    className="bg-black text-white"
+                  >
+                    <IoClose />
+                  </div>
+                </div>
+                <button
+                  className="bg-black text-white px-4 py-2 rounded select-none w-full border-t-1 border-b-1 border-gray-500 flex gap-x-6 align-middle items-center"
+                  onClick={() => handleButtonClickNavigate("home")}
+                >
+                  <FaHome size={23} /> <text>Home</text>
+                </button>
+                <button
+                  className="bg-black text-white px-4 py-2 rounded select-none w-full border-t-1 border-b-1 border-gray-500 flex gap-x-6 align-middle items-center"
+                  onClick={() => handleButtonClickNavigate("Skills")}
+                >
+                  <RxBarChart />
+                  {button1}
+                </button>
+                <button
+                  className="bg-black text-white px-4 py-2 rounded select-none w-full border-t-1 border-b-1 border-gray-500 flex gap-x-6 align-middle items-center"
+                  onClick={() => handleButtonClickNavigate("Experiences")}
+                >
+                  <GrAchievement />
+                  {button2}
+                </button>
+                <button
+                  className="bg-black text-white px-4 py-2 rounded select-none w-full border-t-1 border-b-1 border-gray-500 flex gap-x-6 align-middle items-center"
+                  onClick={() => handleButtonClickNavigate("Projects")}
+                >
+                  <TbFolderCode size={23} />
+                  {button3}
+                </button>
               </div>
-            </div>
-            <button
-              className="bg-black text-white px-4 py-2 rounded select-none w-full border-t-1 border-b-1 border-gray-500 flex gap-x-6 align-middle items-center"
-              onClick={() => handleButtonClickNavigate("home")}
-            >
-              <FaHome size={23} /> <text>Home</text>
-            </button>
-            <button
-              className="bg-black text-white px-4 py-2 rounded select-none w-full border-t-1 border-b-1 border-gray-500 flex gap-x-6 align-middle items-center"
-              onClick={() => handleButtonClickNavigate("Skills")}
-            >
-              <RxBarChart />
-              {button1}
-            </button>
-            <button
-              className="bg-black text-white px-4 py-2 rounded select-none w-full border-t-1 border-b-1 border-gray-500 flex gap-x-6 align-middle items-center"
-              onClick={() => handleButtonClickNavigate("Experiences")}
-            >
-              <GrAchievement />
-              {button2}
-            </button>
-            <button
-              className="bg-black text-white px-4 py-2 rounded select-none w-full border-t-1 border-b-1 border-gray-500 flex gap-x-6 align-middle items-center"
-              onClick={() => handleButtonClickNavigate("Projects")}
-            >
-              <TbFolderCode size={23} />
-              {button3}
-            </button>
-          </div>
-        )}
-      </div>
+            )}
+
     </div>
   );
 };
--- src/components/ui/Avatar/Avatar.tsx
+++ src/components/ui/Avatar/Avatar.tsx
@@ -1,26 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Avatar from "@radix-ui/react-avatar";
 
-const AvatarDemo = () => (
-  <div className="flex gap-5 select-none">
-   <a href="https://github.com/HenriqueFMA" target="_blank" draggable="false">
-   <Avatar.Root className="flex items-center justify-center overflow-hidden w-16 h-16 rounded-full bg-gray-200">
-      <Avatar.Image
-        className="w-full h-full object-cover rounded-full"
-        src="https://avatars.githubusercontent.com/u/106880173?v=4"
-        alt="Colm Tuite"
-        draggable="false"
-      />
-      <Avatar.Fallback
-        className="flex items-center justify-center text-white bg-gray-600 text-sm font-medium"
-      >
-        CT
-      </Avatar.Fallback>
-    </Avatar.Root>
-   </a>
+const AvatarDemo = () => {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [showMessage, setShowMessage] = useState(false);
 
-   
-  </div>
-);
+  const handleMouseMove = (e: React.MouseEvent) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseEnter = () => {
+    setShowMessage(true); // Mostrar mensagem ao passar o mouse
+  };
+
+  const handleMouseLeave = () => {
+    setShowMessage(false); // Esconder mensagem quando o mouse sair
+  };
+
+  return (
+    <div
+      className="flex gap-5 select-none relative"
+      onMouseMove={handleMouseMove}
+    >
+      <a
+        href="https://github.com/HenriqueFMA"
+        target="_blank"
+        draggable="false"
+        onMouseEnter={handleMouseEnter} 
+        onMouseLeave={handleMouseLeave} 
+      >
+        <Avatar.Root className="flex items-center justify-center overflow-hidden w-16 h-16 rounded-full border-blue-500 hover:border-2 bg-gray-200">
+          <Avatar.Image
+            className="w-full h-full object-cover rounded-full"
+            src="https://avatars.githubusercontent.com/u/106880173?v=4"
+            alt="Colm Tuite"
+            draggable="false"
+          />
+          <Avatar.Fallback
+            className="flex items-center justify-center text-white bg-gray-600 text-sm font-medium"
+          >
+            CT
+          </Avatar.Fallback>
+        </Avatar.Root>
+      </a>
+
+    
+      
+
+      {showMessage && (
+        <div 
+          style={{
+            left: `${cursorPosition.x - 390}px`, 
+            top: `${cursorPosition.y - 5}px`, 
+          }}
+          className="absolute text-white w-28 align-middle items-center bg-gray-950 px-3 py-1 rounded-md text-sm transition-opacity duration-150 ease-in-out"
+        >
+          Go to Github
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default AvatarDemo;
