3078e78330d9daf8bb5ee8b7d9c5bb8bb49ee166
HenriqueFMA <carolinahenriquef@gmail.com>
2024-12-23 10:51:33 -0300

    add TextProjects module and enhance Projects page with dynamic language support for experiences

---
+++ src/lib/TextProjects.tsx
@@ -0,0 +1,44 @@
+export const texts = {
+    PT: {
+      title: "Experiências",
+      experiences: [
+        {
+          title: "  Implementação-de-autonomos AFD,AFNe e AFNe",
+          description: [
+            "implementação de Autômatos Finitos Determinísticos (AFD), Autômatos Finitos Não-Determinísticos (AFN) (usando backtrack) e Autômatos Finitos Não-Determinísticos com Transições vazias (AFNe) (usando paralelismo). ",
+          ],
+          link:"https://github.com/HenriqueFMA/Implementacao-de-automato-AFD-AFN-AFNe"
+        },
+        {
+            title: "DNS Game",
+            description: [
+              "Um jogo de divulgação científica com o objetivo de ensinar conceitos de redes, como DNS, IP e roteamento. Além disso, incorpora elementos de criptografia e hacking de maneira lúdica e envolvente."
+            ],
+            link:"https://github.com/ArthurNobrega1/dns-game/issues"
+          },
+        
+      ],
+    },
+    EN: {
+      title: "Experience",
+      experiences: [
+        {
+          title: "Implementation of Finite Automata: AFD, AFN, and AFNe",
+         
+          description: [
+            "Implementation of Deterministic Finite Automata (DFA), Non-Deterministic Finite Automata (NFA) (using backtracking), and Non-Deterministic Finite Automata with ε-Transitions (NFA-epsilon) (using parallelism).",
+          ],
+          link:"https://github.com/HenriqueFMA/Implementacao-de-automato-AFD-AFN-AFNe"
+
+        },
+        {
+            title: "DNS Game",
+            description: [
+              "A science communication game designed to teach network concepts such as DNS, IP, and routing. It also incorporates elements of cryptography and hacking in a playful and engaging way."
+            ],
+            link:"https://github.com/ArthurNobrega1/dns-game/issues"
+          },
+      ],
+    },
+  };
+  
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LanguageNameContext } from "../lib/LangueageProvider";
+import { Card, CardTitle, CardDescription } from "../components/ui/Card";
+import { texts } from "../lib/TextProjects";
+import { FaGithub } from "react-icons/fa";
 
-const Projects = () => (
-    <>
-   <div>
-    <h1 className="text-white"> Projects</h1>
-   </div>
-    </>
-);
+const Projects = () => {
+  const { name } = useContext(LanguageNameContext);
+  const languageTexts = texts[name] || texts.EN;
+
+  return (
+    <div className="mt-32 text-white select-none">
+      <h2 className="m-16 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        {languageTexts.title}
+      </h2>
+
+      <div className="flex flex-wrap justify-center gap-8">
+        {languageTexts.experiences.map((experience, index) => (
+          <a
+            key={index}
+            href={experience.link} 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="no-underline"
+          >
+            <Card className="flex flex-col p-8 w-[500px] h-[450px] items-center justify-between hover:bg-neutral-950 hover:scale-110 hover:shadow-sm transition-all gap-4">
+              <div className="flex flex-col flex-grow">
+                <CardTitle className="scroll-m-20 text-4xl font-extrabold tracking-tight text-white lg:text-3xl text-center">
+                  {experience.title}
+                </CardTitle>
+                <CardDescription className="text-xl font-semibold text-gray-300 text-center">
+                  {experience.institution}
+                </CardDescription>
+                <span className="text-gray-400 text-sm">{experience.period}</span>
+                <ul className="text-white text-justify list-disc list-inside mt-4">
+                  {experience.description.map((item, idx) => (
+                    <li key={idx}>{item}</li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="mt-auto flex justify-center w-full">
+                <FaGithub className="text-4xl text-white" />
+              </div>
+            </Card>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default Projects;
